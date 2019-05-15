4b49448b0a2002e66b7db508dab038053d5571d0
Eyeden <iden.watanabe@gmail.com>
2019-05-15 13:57:54 -1000

    Update app.py

--- app.py
+++ app.py
@@ -21,6 +21,7 @@ from statistics import mean, median
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
 # app.config['suppress_callback_exceptions'] = True
 
 
